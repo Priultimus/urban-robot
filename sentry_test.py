d32071bc28f1b106cd81b41bd6f65b68041cf761
Priultimus <29077634+Priultimus@users.noreply.github.com>
2021-04-03 13:11:50 -0400

    man this is annoying

--- sentry_test.py
+++ sentry_test.py
@@ -7,7 +7,7 @@ load_dotenv(verbose=True)
 SENTRY_LINK = os.environ.get("SENTRY_LINK")
 
 sentry_sdk.init(
-    SENTRY_LINK, release="urban-robot@0.0.1-alpha.1", traces_sample_rate=1.0
+    SENTRY_LINK, release="urban-robot@0.0.1-alpha.3", traces_sample_rate=1.0
 )
 
 division_by_zero = 1 / 0
