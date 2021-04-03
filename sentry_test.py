a9926fb3d177c2c9b84bf100ef53c1242f764099
Priultimus <29077634+Priultimus@users.noreply.github.com>
2021-04-03 13:04:32 -0400

    update version

--- sentry_test.py
+++ sentry_test.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 SENTRY_LINK = os.environ.get("SENTRY_LINK")
 
-sentry_sdk.init(SENTRY_LINK, release="UrbanRobotTest@0.0.1", traces_sample_rate=1.0)
+sentry_sdk.init(SENTRY_LINK, release="urban-robot@0.0.1", traces_sample_rate=1.0)
 
 division_by_zero = 1 / 0
